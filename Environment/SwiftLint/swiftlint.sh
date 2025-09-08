74a1f5ca5a6d3380ff029d3b6213325bd6f60c1e
anx1senwy <romqaaa1337@gmail.com>
2025-09-08 19:31:53 +0300

    [build] Update SwiftLint configuration

--- Environment/SwiftLint/swiftlint.sh
+++ Environment/SwiftLint/swiftlint.sh
@@ -10,7 +10,20 @@ fi
 if [ -f "$SWIFTLINT_PATH" ]; then
     echo "[*] Starting code scan with linter for configuration: $CONFIGURATION."
     
-    "$SWIFTLINT_PATH" lint
+    cd Environment/SwiftLint/
     
+    if [ ! -f ".swiftlint.yml" ]; then
+        echo "[!] Configuration file .swiftlint.yml not found."
+        exit 1
+    fi
+
+    "$SWIFTLINT_PATH" lint --config .swiftlint.yml
+	"$SWIFTLINT_PATH" --config .swiftlint.yml --fix
+        
+    if [ $? -ne 0 ]; then
+        echo "[!] SwiftLint encountered an error during scanning."
+        exit 1
+    fi
+
     echo "[*] Scan completed."
 fi
